f903085e33b2602d8e2b5b8d92c6193cfaabae1a
Luis Barrios <luisbarrios.0983@gmail.com>
2022-01-05 01:17:51 -0300

    Cambios en App agregado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import NavBar from './components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/Home/Home';
+import Nosotros from './components/Nosotros/Nosotros';
+import Servicios from './components/Servicios/Servicios';
+import Productos from './components/productos/ItemListContainer/ItemListContainer.jsx'
+import Galeria from './components/galeria/GaleriaListContainer/GaleriaListContainer';
+import Contacto from './components/Contacto/Contacto';
+import ItemDetailContainer from './components/productos/ItemDetailContainer/ItemDetailContainer';
+import CartContextProvider from './context/CartContext'
+import ItemCartContext from './components/carritoCompras/ItemCartContext/ItemCartContext';
+import TerminarCompra from './components/carritoCompras/TerminarCompra/TerminarCompra';
+import Footer from './components/Footer/Footer';
+import Display404 from './components/404/Display404';
+import FormularioCompra from './components/carritoCompras/FormularioCompra/FormularioCompra.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <CartContextProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path='/' element= { <Home/> } />
+          <Route exact path='/nosotros' element= { <Nosotros/> } />
+          <Route exact path='/servicios' element= { <Servicios/> } />
+          <Route exact path='/productos/:categoriaId' element= { <Productos/> } />
+          <Route exact path='/detalle/:id' element= { <ItemDetailContainer/> } />
+          <Route exact path='/galeria' element= { <Galeria/> } />
+          <Route exact path='/contacto' element= { <Contacto/> } />
+          <Route exact path='/cart' element= { <ItemCartContext/> } />
+          <Route exact path='/terminarCompra' element= { <TerminarCompra/> } />
+          <Route exact path='/formularioCompra' element= { <FormularioCompra/> } />            
+          <Route exact path='/display404' element= { <Display404/> } />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </CartContextProvider>
+
   );
 }
 
 export default App;
+
